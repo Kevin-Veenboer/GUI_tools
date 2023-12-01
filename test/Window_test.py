6a43cdd0f091abc6011086a2eebbf9776349b0b0
Kevin-Veenboer <135600479+Kevin-Veenboer@users.noreply.github.com>
2023-12-01 19:57:16 +0100

    created a progress bar test

--- test/Window_test.py
+++ test/Window_test.py
@@ -1,6 +1,6 @@
 import sys
 
-from PySide6 import QtWidgets
+from PySide6 import QtWidgets, QtGui
 import time
 
 
@@ -12,24 +12,37 @@ class UserInterface(QtWidgets.QMainWindow):
         self.setCentralWidget(central_widget)
 
         # Adding Vertial layout
-        vbox = QtWidgets.QVBoxLayout(central_widget)
+        self.vbox = QtWidgets.QVBoxLayout(central_widget)
         self.textBox = QtWidgets.QTextEdit()
-        vbox.addWidget(self.textBox)
+        self.vbox.addWidget(self.textBox)
 
         # Horizontal box for buttons
         hbox = QtWidgets.QHBoxLayout()
-        vbox.addLayout(hbox)
+        self.vbox.addLayout(hbox)
         self.start_button = QtWidgets.QPushButton("Start")
         self.clear_button = QtWidgets.QPushButton("Clear")
         hbox.addWidget(self.start_button)
         hbox.addWidget(self.clear_button)
+
+        # Test progress bar
+        self.progressBar = QtWidgets.QProgressBar()
+        self.progressBar.setMinimum(0)
+        self.progressBar.setMaximum(4)
 
         # Connecting siganls
         self.clear_button.clicked.connect(self.textBox.clear)
         self.start_button.clicked.connect(self.working)
 
     def working(self):
-        self.textBox.append("Work is done")
+        self.vbox.addWidget(self.progressBar)
+
+        for val in range(0, 5):
+            self.progressBar.setValue(val)
+            self.textBox.append("Work is done")
+            time.sleep(0.5)
+
+    def external_func(self):
+        pass
 
 
 def main():
